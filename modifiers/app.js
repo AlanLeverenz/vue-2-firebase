b9e68f14b0a98a8829a599e1c4864d7db8349e43
Alan Leverenz <awleverenz@aol.com>
2021-08-30 22:57:38 -0400

    event modifiers

---
+++ modifiers/app.js
@@ -0,0 +1,14 @@
+// the Vue instance
+
+new Vue({
+  el: '#app',
+  data: {
+    title: 'Becoming a Vue developer',
+    name: 'Alan'
+  },
+  methods: {
+    logMessage() {
+    console.log('hello world')
+    }
+  }
+})
