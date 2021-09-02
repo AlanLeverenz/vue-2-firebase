25083b862df6ffd4ba3f591e4e7b6cbb57e3ea5e
Alan Leverenz <awleverenz@aol.com>
2021-09-01 20:29:59 -0400

    v-for loops

---
+++ for-loops/app.js
@@ -0,0 +1,19 @@
+// the Vue instance
+
+new Vue({
+  el: '#app',
+  data: {
+    title: 'Becoming a Vue developer',
+    items: ['Mushroom','Green Shells','Red Shells','Banana','Star'],
+    ninjas: [
+      { name: 'Crystal', age: 25, belt: 'Black'},
+      { name: 'Tess', age: 31, belt: 'Brown'},
+      { name: 'Eric', age: 32, belt: 'Orange'}
+    ]
+  },
+  methods: { 
+
+  }
+})
+
+  
