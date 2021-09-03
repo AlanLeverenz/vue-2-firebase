004ba557e0499dd0477895685631bc8a34f1a988
Alan Leverenz <awleverenz@aol.com>
2021-09-02 22:26:11 -0400

    arrow functions 2

--- arrow-functions/arrow-functions.js
+++ arrow-functions/arrow-functions.js
@@ -10,6 +10,12 @@ const greetThree = (name,age) => {
   console.log('hello ' + name + ', you are ' + age + ' years old.')
 } 
 
+// as a callback function
+// object.method(name => {
+//   console.log('hello ' + name)
+// })
+
+
 greetOne('mario')
 greetTwo('alan')
 greetThree('tess',32)
