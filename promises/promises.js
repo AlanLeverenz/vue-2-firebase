d8404829fad4fc1c0e463041765185bc3d3d5893
Alan Leverenz <awleverenz@aol.com>
2021-09-03 23:37:17 -0400

    promises with catch

---
+++ promises/promises.js
@@ -0,0 +1,8 @@
+var axios = require('axios')
+
+axios.get('https://jsonplaceholder.typicode.com/posts/1').then((response) => {
+  console.log(response.data)
+}).catch((err) => {
+  console.log(err)
+})
+
