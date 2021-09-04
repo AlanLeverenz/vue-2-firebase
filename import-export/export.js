8c57888457053ad1419aae2271bd552ed7e78118
Alan Leverenz <awleverenz@aol.com>
2021-09-04 11:26:36 -0400

    import export

---
+++ import-export/export.js
@@ -0,0 +1,13 @@
+export default 'hello'
+
+// export default {
+//   name: 'mario',
+//   age: 32
+// }
+
+// var name = 'luigi'
+// var age = 30
+// var favColour = 'green'
+
+// export { name, age, favColour }
+
